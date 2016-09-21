9f817bf3a5365c4f483ba8432439eabf89a3ddbc
Geoff Miles <miles.geoff@gmail.com>
2016-09-20 23:10:53 -0400

    Sign Up and Mailer Confirmation Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: 'home#index'
   get 'home/index'
 
   get 'home/about'
-
-  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
