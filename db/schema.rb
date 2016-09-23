2bc642a876afd4c8b20d53a667cf0cffd8e18e8c
Geoff Miles <miles.geoff@gmail.com>
2016-09-23 08:17:01 -0400

    Rollback and Remigration

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,14 @@
 ActiveRecord::Schema.define(version: 20160921032150) do
 
   create_table "registered_applications", force: :cascade do |t|
+    t.string   "url"
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "registered_applications", ["user_id"], name: "index_registered_applications_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
