7dbd8aeb01d71e573d346c70eb81b5f55d7e8c53
Geoff Miles <miles.geoff@gmail.com>
2016-09-25 21:14:28 -0400

    API Controller complete

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,5 +1,7 @@
 class API::EventsController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+  
   before_filter :set_access_control_headers
 
   def set_access_control_headers
