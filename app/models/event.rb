2acc0d3d97adb89baa7f8ab670dbfff45e6bcf3c
Geoff Miles <miles.geoff@gmail.com>
2016-10-16 11:42:01 -0400

    Added PG gem

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :registered_application
   has_many :users, through: :registered_applications
+
+  validates :registered_application, presence: true
+  validates :name, presence: true
 end
